57eb09920b3817ad1ff524fe9ab470f5524bed43
845alex845 <alexunmsm2016@gmail.com>
2020-04-02 21:41:22 -0500

    web3

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <header>
+        <div class="textos">
+            <h1 class="titulo">Rommel Alex Rojas Coaquira</h1>
+            <h3 class="subtitulo">Desarrollo de sitios web</h3>
+            <a href="#" class="boton">Suscribete</a>
+        </div>
+        <div class="sesgoabajo"></div>
+    </header>
+    <main>
+        <section class="acerca-de">
+            <div class="contenedor">
+                <h2 class="sobre-nosotros"> Sobre mí</h2>
+                <h3 class="slogan">Tirando codigo en la web </h3>
+                <p class="parrafo">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa unde accusamus harum ipsa eius consectetur laborum voluptate, iusto optio aliquam non voluptatibus totam, nobis excepturi. Distinctio expedita ut dolores labore saepe veritatis
+                    aliquam. Tempora, quaerat? Voluptates explicabo nostrum modi quisquam. Et, nisi id fugiat laboriosam nobis pariatur, laudantium suscipit quae exercitationem, molestiae animi voluptas nesciunt tenetur qui. Dolores, illum veniam?</p>
+                <p class="parrafo">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa unde accusamus harum ipsa eius consectetur laborum voluptate, iusto optio aliquam non voluptatibus totam, nobis excepturi. Distinctio expedita ut dolores labore saepe veritatis
+                    aliquam. Tempora, quaerat? Voluptates explicabo nostrum modi quisquam. Et, nisi id fugiat laboriosam nobis pariatur, laudantium suscipit quae exercitationem, molestiae animi voluptas nesciunt tenetur qui. Dolores, illum veniam?</p>
+                <a href="#" class="boton">Suscribete</a>
+            </div>
+        </section>
+        <section class="galeria">
+            <div class="sesgoarriba"></div>
+            <div class="imagenes none"><img src="img/1.jpg" alt=""></div>
+            <div class="imagenes"><img src="img/3.jpg" alt=""></div>
+            <div class="imagenes"><img src="img/2.jpg" alt="">
+                <div class="encima">
+                    <h2>Alex Design</h2>
+                    <div class=""></div>
+                </div>
+            </div>
+            <div class="imagenes">
+                <img src="img/4.jpeg" alt="">
+
+            </div>
+            <div class="imagenes none"><img src="img/5.jpeg" alt=""></div>
+            <div class="sesgoabajo"></div>
+        </section>
+        <section class="miembros">
+            <div class="contenedor">
+                <h2 class="sobre-nosotros">Mis Frameworks</h2>
+                <h3 class="slogan">Conoce nuestras herramientas de trabajo</h3>
+                <div class="cards">
+                    <div class="card">
+                        <img src="img/angular.png" alt="">
+                        <h4>AngularJS</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                    <div class="card">
+                        <img src="img/vue.png" alt="">
+                        <h4>VueJS</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                    <div class="card">
+                        <img src="img/react2.png" alt="">
+                        <h4>ReactJS</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fondo">
+            <div class="sesgoarriba"></div>
+            <div class="contenedor">
+                <h2 class="titulo-patrocinadores">Nuestros clientes</h2>
+                <h3 class="subtitulo-patrocinadores">conoce a algunos de nuestros patrocinadores</h3>
+                <div class="clientes">
+                    <div class="cliente"><img src="img/heroku.png" alt=""></div>
+                    <div class="cliente"><img src="img/mongo1.png" alt=""></div>
+                    <div class="cliente"><img src="img/firebase.png" alt=""></div>
+                </div>
+                <h3 class="subtitulo-patrocinadores especial">Y mucho más...</h3>
+
+            </div>
+            <div class="sesgoabajo-unico"></div>
+        </section>
+        <footer>
+            <div class="contenedor">
+                <h2 class="titulo-footer">Contáctanos</h2>
+                <h3 class="subtitulo-footer">Lo apreciaremos mucho</h3>
+                <div class="footer">
+                    <form action="">
+                        <input type="text" name="" id="" placeholder="Nombre">
+                        <input type="email" name="" id="" placeholder="E-mail">
+                        <textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                        <input type="submit" value="Enviar">
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </main>
+</body>
+
+</html>
